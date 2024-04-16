6c0163203dc85fff77720ddaf163d3540864b5af
sanbergsamira34@gmail.com <sanbergsamira34@gmail.com>
2024-04-15 23:06:24 -0300

    alterações js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,37 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+const tableForm = document.getElementsByClassName('container-table')[0]
+const form = document.querySelector('form')
+const firstName = document.querySelector('#firstname')
+const lastName = document.querySelector('#lastname')
+const email = document.querySelector('#email')
+const number = document.querySelector('#number')
+const genders = document.getElementsByName('gender')
+const setLocalStorage = (database) =>
+  localStorage.setItem("users", JSON.stringify(database));
+
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem("users")) ?? [];
+
+const readLocalStorage = () => getLocalStorage();
+const createRegistration = (register) => {
+    const db = getLocalStorage();
+    db.push(register);
+    setLocalStorage(db);
+  };
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    let genderInput;
+    let db = readLocalStorage() 
+    for (let radio of genders)
+      if (radio.checked) {
+        genderInput = radio.value
+      }
+    
+    let Matricula = {
+        name: firstName.value + ' ' + lastName.value,
+        email: email.value , 
+        number: number.value ,
+        gender: genderInput 
     }
-}
+    createRegistration(Matricula)
+})
+
